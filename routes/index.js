6e5cfc415d92acf889b6dcd88c4445b7c32bb257
Brad <Bradrockk@gmail.com>
2024-02-13 00:01:07 -0500

    adjusted some routing

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,14 @@ const Routes = require('./api');
 
 router.use('/api', Routes);
 
+router.get('/', (req,res) =>{
+    try{ 
+    res.render('index.hbs')
+    }
+    catch(error){
+        console.log(`An error occured. ${error}`)
+    }
+  })
 
 
 module.exports = router;
