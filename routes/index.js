98f6abf8ddd2fd117006cdf6a5478588023acacf
Brad <Bradrockk@gmail.com>
2024-02-14 14:43:59 -0500

    added inital navbar and css for it

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,19 @@
 const router = require('express').Router();
 const Routes = require('./api');
-
+const path = require('path')
 router.use('/api', Routes);
 
 router.get('/', (req,res) =>{
     try{ 
-    res.render('index.hbs')
+    res.render('index.hbs', {title: 'home', layout: 'main'})
     }
     catch(error){
-        console.log(`An error occured. ${error}`)
+        console.log(`An error occured. As follows: ${error}`)
     }
   })
-
+router.get('/public/css/index.css', (req,res)=>{
+  console.log(__dirname)
+  res.sendFile(path.resolve(__dirname + '../../public/css/index.css'))
+})
 
 module.exports = router;
