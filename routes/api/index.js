e49f044aca2e88e3d23c749639acc9c73aecd72b
Brad <Bradrockk@gmail.com>
2024-02-15 14:03:51 -0500

    simplified post request

--- routes/api/index.js
+++ routes/api/index.js
@@ -8,11 +8,10 @@ router.post('/posts', async (req, res) => {
     const { title, text, time } = req.body;
     const newPost = await posts.create({ title, text, time});
 
-    console.log('New blog post added:', newPost);
     res.status(201).json({ message: 'success!' });
   } catch (error) {
-    console.error('Error inserting blog post:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error(error);
+    res.status(500).json({error});
   }
 });
   
