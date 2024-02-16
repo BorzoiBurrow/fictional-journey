197178a2f2e55dfe83ca37a0fdaa7f35e2142be5
Brad <Bradrockk@gmail.com>
2024-02-15 19:14:21 -0500

    added inital login capability

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,12 @@
 const sequelize = require('../../config/connection');
 const posts = require("../../models/posts")
 const router = require('express').Router();
+const Account = require("../../models/accounts")
+
+// creating the session with express-sessions
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sessionStore = new SequelizeStore({ db: sequelize });
 
 // post for making a new blog post
 router.post('/posts', async (req, res) => {
@@ -15,12 +21,24 @@ router.post('/posts', async (req, res) => {
   }
 });
 
-// post for logging in
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
   const { username, password } = req.body;
-
-
-  res.send('Login successful!');
+console.log(req.body)
+  try {
+    // Find the user by username
+    const user = await Account.findOne({ where: { username } });
+    if (user && bcrypt.compareSync(password, user.password)) {
+      
+// express login
+      req.session.userId = user.id;
+      res.send('Login successful!');
+    } else {
+      res.send('Invalid username or password.');
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
 });
   
 module.exports = router;
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 3001;
 
 app.set('view engine', 'hbs')
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 app.use(routes)
 
 app.listen(PORT, () => {
